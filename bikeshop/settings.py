bac6f316464d4ec612709ddc13694bfb9e03ce73
AMALGSOFT <rivangushu200241>
2023-09-04 15:13:13 +0300

    first run

--- bikeshop/settings.py
+++ bikeshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f=&$ishz26(3)!(7lt5roc^s&68!wedu7(1v=wn5z3i8h%@r*y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['bike-4aacaf3b5ecc.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
