11b7e4d89db98d82026c5ac9d8e4a4c3a552f6b2
AMALGSOFT <rivangushu200241>
2023-09-04 09:32:02 +0300

    first

--- bikeshop/settings.py
+++ bikeshop/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'shop/static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
